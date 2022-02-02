20005a3ed7734ee4720c0542da37e9fd05c80c30
Nathan Black <nblack@quanthub.com>
2022-02-02 17:35:32 -0600

    Update documentation

--- _build/jupyter_execute/1_2_0_data_applications.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Data Applications
-# 
-# Now that you’ve built an awareness of common data sources, you’ll move on to learning how that data is applied in everyday products and services.
-
-# 
--- _build/jupyter_execute/intro.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Welcome to QuantHub Upskill Data Curriculum
-# This is a comprehensive book detailing the data skill content covered in QuantHub Upskill
-
-# :::{note}
-# This book is designed to show you what learning content is covered in QuantHub Upskill. We recommend using the QuantHub Upskill web app or Microsoft Teams bot to learn and practice the data skills covered in this book.
-# :::
